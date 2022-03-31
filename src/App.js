5931ffc71a2c2ae82c9e07c26deaf9d6ca5e9be8
Shahzaib-ur-Rehman <shaahzaibrehman@gmail.com>
2022-03-31 13:21:28 +0500

    working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import "./App.css";
+import Footer from "./components/Footer";
+import Galary from "./components/Galary";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
+import OurTeam from "./components/OurTeam";
+import Testimonial from "./components/Testimonial";
 function App() {
   return (
     <main>
       <Header />
+      <Hero />
+      <Galary />
+      <OurTeam />
+      <Testimonial />
+      <Footer />
     </main>
   );
 }
